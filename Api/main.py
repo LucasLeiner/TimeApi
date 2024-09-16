e0003c119e2cd8a7baa52a9a46e9a1e786772686
LucasLeiner <lucasleiner05@gmail.com>
2024-09-16 10:55:45 +0200

    Ajout endpoints utc et timezone

--- Api/main.py
+++ Api/main.py
@@ -1,15 +1,30 @@
-from typing import Union
 from datetime import datetime
-from fastapi import FastAPI
+from zoneinfo import ZoneInfo
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+@app.get("/Time/UTC")
+def readTime():
+    dateString = datetime.utcnow().strftime("%d/%m/%Y")
+    timeString = datetime.utcnow().strftime("%H:%M:%S")
+    return {"Name": "UTC Time", "Date": dateString, "Time": timeString}
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/Time/{timezone}")
+def get_timezone_time(timezone: str):
+    # Remplacer les "-" par "/"
+    formatted_timezone = timezone.replace("-", "/")
+    
+    try:
+        tz = ZoneInfo(formatted_timezone)
+    except Exception:
+        raise HTTPException(status_code=400, detail="Fuseau horaire invalide")
+
+    now_in_tz = datetime.now(tz)
+    date_string = now_in_tz.strftime("%d/%m/%Y")
+    time_string = now_in_tz.strftime("%H:%M:%S")
+
+    return {"timezone": formatted_timezone, "date": date_string, "time": time_string}
+
