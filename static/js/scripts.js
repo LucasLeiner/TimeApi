51dc2bdb247642580fb2379952418e386e47ca5c
Gaspar Francisco <108736731+Gasparfgf@users.noreply.github.com>
2024-12-02 10:53:05 +0100

    remove updateClock for instance

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -62,19 +62,6 @@ var map = L.map('map').setView([20, 0], 2);
         });
 
 
-function updateClock() {
-    const clock = document.getElementById('clock');
-    const now = new Date();
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    clock.textContent = `${hours}:${minutes}:${seconds}`;
-}
-
-setInterval(updateClock, 1000);
-updateClock();
-
-
 function getWeather(lat, lng) {
     console.log("Appel à getWeather avec les coordonnées :", lat, lng);
     axios.get(`/weather/${lat}/${lng}`)
