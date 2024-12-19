e1d61dee9f9f9db8a3750b45991d11ececda59fa
BradleyKissouna <bradley.kissouna@etu.univ-orleans.fr>
2024-12-19 18:05:10 +0100

    Add sys.path

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,10 @@
-from fastapi.testclient import TestClient
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
 from TimeApi.Api.main import app
+from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
