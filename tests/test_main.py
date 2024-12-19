76029eb8395946bc4c48bc3fd778a96ad2a71a67
BradleyKissouna <bradley.kissouna@etu.univ-orleans.fr>
2024-12-19 18:14:17 +0100

    Test of config path

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,9 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../Api/")))
 
-from TimeApi.Api.main import app
+from main import app
 from fastapi.testclient import TestClient
 
 client = TestClient(app)
